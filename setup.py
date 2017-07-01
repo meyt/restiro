68a7420007d215d42712f5bf00920eec814dbd47
meyt <pasd3000@gmail.com>
2017-07-01 23:09:13 +0430

    replaces

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 
 # reading package's version (same way sqlalchemy does)
-with open(os.path.join(os.path.dirname(__file__), 'apihero', '__init__.py')) as v_file:
+with open(os.path.join(os.path.dirname(__file__), 'webtest_docgen', '__init__.py')) as v_file:
     package_version = re.compile(r".*__version__ = '(.*?)'", re.S).match(v_file.read()).group(1)
 
 setup(
