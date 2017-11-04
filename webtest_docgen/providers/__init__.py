15ec29ae85ddfc1373c4de57fb43e087ad218c78
meyt <pasd3000@gmail.com>
2017-11-04 21:28:35 +0330

    Documentation

--- webtest_docgen/providers/__init__.py
+++ webtest_docgen/providers/__init__.py
@@ -1,3 +1,9 @@
+"""Documentation Providers
+To creating final output of documentation from defined models and 
+tests result, we need providers.
+official providers should not use third-party libraries, but for having
+a "featureful" one, we can inherit the ``BaseProvider`` and implement it.
+"""
 from .base import BaseProvider
 from .json import JSONProvider
 from .markdown import MarkdownProvider
