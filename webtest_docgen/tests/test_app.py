a933f5fab27e6d65cc27895eb902dc550938962f
meyt <pasd3000@gmail.com>
2017-11-08 11:38:37 +0330

    Improve application test with requests

--- webtest_docgen/tests/test_app.py
+++ webtest_docgen/tests/test_app.py
@@ -1,16 +1,24 @@
 import unittest
 
-from webtest_docgen.tests.helpers import WebAppTestCase
+from webtest_docgen.tests.helpers import WebAppTestCase, mockup_resources
 
 
 class AppTestCase(WebAppTestCase):
 
     def test_app(self):
-        resp = self.wsgi_app.get('/user', 'a=b')
-        resp.mustcontain('a=b')
+        # Simple request
+        resp_one = self.wsgi_app.get('/user', 'a=b')
+        resp_one.mustcontain('a=b')
 
-        resp = self.wsgi_app.get('/user', 'b=c')
-        resp.mustcontain('b=c')
+        # Mockup
+        self.docs_root.set_resources(*mockup_resources())
+
+        # Fill examples by request
+        self.wsgi_app.get('/user')
+        resource = self.docs_root.resources.find(path='/user', method='get')
+        self.assertEqual(len(resource.examples), 1)
+        self.assertEqual(resource.examples[0].response.status, 200)
+        self.assertEqual(resource.examples[0].response.body_format, None)
 
 if __name__ == '__main__':  # pragma: nocover
     unittest.main()
