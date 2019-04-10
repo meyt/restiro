615a200cfb384eb91f509bd5e89c3e4bf15a86d0
meyt <pasd3000@gmail.com>
2019-04-11 01:21:15 +0430

    Add CLI locales build

--- restiro/cli/__init__.py
+++ restiro/cli/__init__.py
@@ -1,8 +1,11 @@
 import argparse
 from importlib.util import find_spec
 
-from os.path import dirname
+from os import scandir, makedirs
+from os.path import dirname, isdir, basename, join
+
 from restiro import Documentor
+from restiro.helpers import validate_locale_name
 
 parser = argparse.ArgumentParser(description='Restiro Builder')
 parser.add_argument(
@@ -14,21 +17,54 @@ parser.add_argument(
 parser.add_argument(
     '-b', '--base-uri', help='Base URI')
 parser.add_argument(
-    '-g', '--generator', choices=('markdown', 'json'),
+    '-g', '--generator', choices=('markdown', 'json', 'spa_material'),
     default='markdown', help='Generator, default: markdown')
+parser.add_argument(
+    '-l', '--locales', default='./locales', help='Locales directory')
+parser.add_argument(
+    '--build-gettext', default=False, const=True, nargs='?',
+    help='Build .POT templates')
 
 
 def main():
     args = parser.parse_args()
-    title = args.t or args.src
+    title = args.title or args.src
     source_dir = dirname(find_spec(args.src).origin)
+    locales_dir = args.locales
+    output_base_dir = args.output
 
-    documentor = Documentor(
-        title=title,
-        base_uri=args.b,
-        source_dir=source_dir,
-        output_dir=args.o,
-        generator_type=args.g
-    )
+    def get_documentor():
+        return Documentor(
+            title=title,
+            base_uri=args.base_uri,
+            source_dir=source_dir,
+            generator_type=args.generator
+        )
 
-    documentor.generate()
+    if args.build_gettext:
+        gettext_dir = join(locales_dir, 'gettext')
+        makedirs(gettext_dir, exist_ok=True)
+
+        get_documentor().generate_gettext(gettext_dir)
+        print('gettext .POT templates generated in: %s' % gettext_dir)
+        return
+
+    if isdir(locales_dir):
+        for entry in scandir(locales_dir):
+            if not entry.is_dir():
+                continue
+
+            locale = basename(entry.path)
+
+            if not validate_locale_name(locale):
+                continue
+
+            output_dir = join(output_base_dir, locale)
+            makedirs(output_dir, exist_ok=True)
+            get_documentor().generate(output_dir, locales_dir, locale)
+
+            print('Documentation build success. (%s)' % output_dir)
+
+    else:
+        get_documentor().generate(output_base_dir)
+        print('Documentation build success. (%s)' % output_base_dir)
--- restiro/tests/test_documentor.py
+++ restiro/tests/test_documentor.py
@@ -1,26 +1,159 @@
+import pytest
+import locale as lib_locale
+
 from os import makedirs
 from os.path import join
 
 from restiro import Documentor
-from restiro.tests.helpers import stuff_dir, package_dir
+from restiro.helpers import validate_locale_name
+from restiro.tests.helpers import stuff_dir, temp_dir
 
-temp_dir = join(package_dir, 'temp')
 
+# FIXME: empty spaces and new lines are redundant
+excepted_pot = """msgid "Online Store"
+msgstr ""
 
-def get_destination_dir(provider_name):
-    result = join(temp_dir, 'temp', '%s_generator' % provider_name)
-    makedirs(result)
-    return result
+msgid "Get sellers list"
+msgstr ""
+
+msgid "
+"
+msgstr ""
+
+msgid "Get a seller"
+msgstr ""
+
+msgid "
+"
+msgstr ""
+
+msgid "Delete a seller"
+msgstr ""
+
+msgid "
+"
+msgstr ""
+
+msgid "Get all products"
+msgstr ""
+
+msgid "
+"
+msgstr ""
+
+msgid ""
+msgstr ""
+
+msgid ""
+msgstr ""
+
+msgid ""
+msgstr ""
+
+msgid "Get a seller"
+msgstr ""
+
+msgid "
+"
+msgstr ""
+
+msgid "Update a product"
+msgstr ""
+
+msgid "
+"
+msgstr ""
+
+msgid ""
+msgstr ""
+
+msgid " Product Model "
+msgstr ""
+
+msgid " Can purchase this product"
+msgstr ""
+
+msgid " When product manufactured
+"
+msgstr ""
+
+msgid "Delete a product with product ID, but actually its marked as deleted. 
+After review the product can delete permanently. 
+List of products cannot delete: - Products purchased on time - Products related to a `seller`"
+msgstr ""
+
+msgid "Delete a product"
+msgstr ""
+
+msgid " Product ID
+"
+msgstr ""
+
+msgid " Access Token
+"
+msgstr ""
+
+"""
 
 
 def test_documentor():
     online_store_dir = join(stuff_dir, 'online_store')
 
+    destination_dir = join(temp_dir, 'markdown_generator')
+    makedirs(destination_dir, exist_ok=True)
+
     documentor = Documentor(
         title='Online Store',
         source_dir=online_store_dir,
-        output_dir=get_destination_dir('markdown'),
         generator_type='markdown'
     )
 
-    documentor.generate()
+    # Try with an invalid generator
+    with pytest.raises(ValueError):
+        Documentor(
+            title='Online Store',
+            source_dir=online_store_dir,
+            generator_type='blabla'
+        ).generate(destination_dir)
+
+    # Generate output
+    documentor.generate(destination_dir)
+
+    # Generate output in supported locales
+    locales_dir = join(temp_dir, 'locales')
+    makedirs(locales_dir, exist_ok=True)
+
+    def add_translation(locale):
+        import polib
+        locale_dir = join(locales_dir, locale)
+        messages_dir = join(locale_dir, 'LC_MESSAGES')
+        makedirs(messages_dir)
+        locale_po_file = join(messages_dir, 'restiro.po')
+        locale_mo_file = join(messages_dir, 'restiro.mo')
+        with open(locale_po_file, 'w') as f:
+            f.write('')
+        po = polib.pofile(locale_po_file)
+        po.save_as_mofile(locale_mo_file)
+
+    add_translation('en_US')
+    add_translation('fa_IR')
+    add_translation('un_kn')
+    documentor.generate(destination_dir, locales_dir, 'en_US')
+    documentor.generate(destination_dir, locales_dir, 'fa_IR')
+    with pytest.raises(lib_locale.Error):
+        documentor.generate(destination_dir, locales_dir, 'un_kn')
+
+    # Extract translations template file
+    documentor.generate_gettext(temp_dir)
+
+    with open(join(temp_dir, 'restiro.pot'), 'r') as f:
+        pot_source = ''.join(f.readlines())
+        assert pot_source == excepted_pot
+
+
+def test_validate_locale_name():
+    assert validate_locale_name('en_US')
+    assert validate_locale_name('gsw_FR')
+    assert not validate_locale_name('gsw-FR')
+    assert not validate_locale_name('111')
+    assert not validate_locale_name('00_00')
