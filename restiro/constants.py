cffe2a2adb359d1607ab6a6e6cfd0a427cd87533
meyt <pasd3000@gmail.com>
2019-07-24 09:49:23 +0430

    Fix "Missed path name", closes #24

--- restiro/constants.py
+++ restiro/constants.py
@@ -4,7 +4,7 @@ docstring_block_regex = re.compile(r'\"\"\"([\s\S]*?)\"\"\"')
 within_parentheses_regex = re.compile(r'\(([\s\S]*?)\)')
 within_brackets_regex = re.compile(r'{([\s\S]*?)}')
 single_word_regex = re.compile(r'\s(\[?\w+\]?)(?=\s?)')
-path_regex = re.compile(r'\s/([/\w:]+)(?=\s?)')
+path_regex = re.compile(r'\s/([/\w:]*)(?=\s?)')
 
 
 ansi_orange_fg = '\033[33m'
